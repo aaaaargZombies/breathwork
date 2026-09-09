21713dc9dba06e1c9ed6c1174b2df05d2d8a988c
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2026-09-09 12:37:34 +0100

    feat: integrate Pattern to UI

--- elm/Main.elm
+++ elm/Main.elm
@@ -1,50 +1,108 @@
 module Main exposing (main)
 
 import Browser
+import Data.Pattern as Pattern exposing (Pattern)
 import Html exposing (Html, button, div, text)
+import Html.Attributes
 import Html.Events exposing (onClick)
 import Remote
 
 
 type alias Model =
-    { count : Int }
+    Pattern
 
 
 initialModel : flags -> ( Model, Cmd Msg )
 initialModel _ =
-    ( { count = 0 }, Cmd.none )
+    ( Pattern.init, Cmd.none )
 
 
 type Msg
-    = Play
-    | Stop
+    = USerPressedPlay
+    | UserPressedStop
+    | UserSetBreatheIn String
+    | UserSetBreatheOut String
+    | USerSetHoldIn String
+    | UserSetPauseOut String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Play ->
+        USerPressedPlay ->
             ( model
             , Remote.Play
                 |> Remote.outgoingValue
                 |> Remote.outgoing
             )
 
-        Stop ->
+        UserPressedStop ->
             ( model
             , Remote.Stop
                 |> Remote.outgoingValue
                 |> Remote.outgoing
             )
 
+        UserSetBreatheIn choice ->
+            let
+                model_ : Model
+                model_ =
+                    makeChoice Pattern.withBreatheIn choice model
+            in
+            ( model_, Cmd.none )
+
+        UserSetBreatheOut choice ->
+            let
+                model_ : Model
+                model_ =
+                    makeChoice Pattern.withBreatheOut choice model
+            in
+            ( model_, Cmd.none )
+
+        USerSetHoldIn choice ->
+            let
+                model_ : Model
+                model_ =
+                    makeChoice Pattern.withHoldIn choice model
+            in
+            ( model_, Cmd.none )
+
+        UserSetPauseOut choice ->
+            let
+                model_ : Model
+                model_ =
+                    makeChoice Pattern.withPauseOut choice model
+            in
+            ( model_, Cmd.none )
+
 
 view : Model -> Html Msg
 view model =
     div []
-        [ div []
-            [ button [ onClick Play ] [ text "Play" ]
-            , button [ onClick Stop ] [ text "Stop" ]
+        [ Html.div []
+            [ phaseView { msg = UserSetBreatheIn, get = Pattern.getBreatheIn, label = "Breathe In", pattern = model }
+            , phaseView { msg = USerSetHoldIn, get = Pattern.getHoldIn, label = "Hold In", pattern = model }
+            , phaseView { msg = UserSetBreatheOut, get = Pattern.getBreatheOut, label = "Breathe Out", pattern = model }
+            , phaseView { msg = UserSetPauseOut, get = Pattern.getPauseOut, label = "Pause Out", pattern = model }
             ]
+        , div []
+            [ button [ onClick USerPressedPlay ] [ text "Play" ]
+            , button [ onClick UserPressedStop ] [ text "Stop" ]
+            ]
+        ]
+
+
+phaseView : { msg : String -> Msg, get : Pattern -> Int, pattern : Pattern, label : String } -> Html Msg
+phaseView { msg, get, pattern, label } =
+    let
+        val =
+            pattern |> get |> String.fromInt
+    in
+    Html.label []
+        [ Html.text label
+        , Html.input
+            [ Html.Attributes.type_ "number", Html.Attributes.value val, Html.Events.onInput msg ]
+            []
         ]
 
 
@@ -56,3 +114,19 @@ main =
         , update = update
         , subscriptions = \_ -> Sub.none
         }
+
+
+makeChoice : (Int -> Pattern -> Pattern) -> String -> Pattern -> Pattern
+makeChoice withA userInput pattern =
+    let
+        n : Int
+        n =
+            userInput
+                |> String.toInt
+                |> Maybe.withDefault 0
+
+        pattern_ : Model
+        pattern_ =
+            withA n pattern
+    in
+    pattern_
