8b241c7b8cac17b21baf429313ca86737f8db318
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2026-09-23 13:44:36 +0100

    feat: use vitest expect instead of throwing errors in TS tests

--- ts/main.test.ts
+++ ts/main.test.ts
@@ -1,4 +1,4 @@
-import { test } from "vitest";
+import { test, expect } from "vitest";
 import * as Hegel from "@hegeldev/hegel";
 import * as Generator from "@hegeldev/hegel/generators";
 
@@ -6,11 +6,8 @@ test("An array reversed twice is the same", () =>
   Hegel.test((testCase) => {
     const vec1 = testCase.draw(Generator.arrays(Generator.integers()));
     const vec2 = [...vec1].reverse().reverse();
-    if (JSON.stringify(vec1) !== JSON.stringify(vec2)) {
-      throw new Error(
-        `reverse mismatch: ${JSON.stringify(vec1)} != ${JSON.stringify(vec2)}`,
-      );
-    }
+
+    expect(vec1).toEqual(vec2);
   }));
 
 const add = (a: number, b: number): number => a + b;
@@ -19,9 +16,5 @@ test("Addition", () =>
   Hegel.test((testCase) => {
     const a = testCase.draw(Generator.integers());
     const b = testCase.draw(Generator.integers());
-    if (add(a, b) !== add(b, a)) {
-      throw new Error(
-        `order of addition shouldn't matter: ${a} + ${b} vs ${b} + ${a}`,
-      );
-    }
+    expect(add(a, b)).toEqual(add(b, a));
   }));
